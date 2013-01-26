50ad138801ee23464d4a618ca292e3358650159c
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-01-25 22:17:14 -0300

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -72,11 +72,10 @@ describe "User pages" do
 
     before do
       sign_in user
-      visit new_user_path
+      get new_user_path
     end
 
-    # specify { response.should redirect_to(root_path) }
-    it { should have_selector('h1',    text: "Welcome to the Sample App") }
+    specify { response.should redirect_to(root_path) }
 
   end
 
@@ -89,7 +88,6 @@ describe "User pages" do
       post users_path(user)
     end
 
-    # it { should have_selector('h1',    text: "Welcome to the Sample App") }
     specify { response.should redirect_to(root_path) }
 
   end
@@ -97,10 +95,20 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+
   end
 
   describe "signup page" do
